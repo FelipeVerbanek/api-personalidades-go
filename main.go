8a20d6b4757dec6f969cf9ff66c8e6c5cd042deb
Felipe <felipe.verbanek@tecnospeed.com.br>
2023-02-11 11:59:56 -0300

    add middleware

--- main.go
+++ main.go
@@ -3,15 +3,11 @@ package main
 import (
 	"fmt"
 	"go-rest-api/database"
-	"go-rest-api/models"
 	"go-rest-api/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Jose", Historia: "Tatata"},
-		{Id: 2, Nome: "Joao", Historia: "Tatata"},
-	}
+
 	fmt.Println("Conectando ao banco de dados!")
 	database.ConectaComBancoDados()
 
